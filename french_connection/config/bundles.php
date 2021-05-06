bb2699f886f121ae9675da2cea997444eb76a49b
ThomasLutaster <lutaster.t@gmail.com>
2021-05-06 16:17:39 +0200

    build : install nelmioCorsBundle --DONE

--- french_connection/config/bundles.php
+++ french_connection/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
