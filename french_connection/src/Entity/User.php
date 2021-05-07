c12da430f7282c2777c6471e0cda0078a3ba735a
ThomasLutaster <lutaster.t@gmail.com>
2021-05-07 15:22:48 +0200

    feat: add lastname and firstname properties in user entity --DONE

--- french_connection/src/Entity/User.php
+++ french_connection/src/Entity/User.php
@@ -126,6 +126,16 @@ class User implements UserInterface
      */
     private $travels;
 
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastname;
+
     public function __construct()
     {
         $this->createdAt = new \DateTime();
@@ -466,6 +476,30 @@ class User implements UserInterface
                 $travel->setUser(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(?string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(?string $lastname): self
+    {
+        $this->lastname = $lastname;
 
         return $this;
     }
