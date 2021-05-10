584220616b3684b05bf6d6fa77b613a92b931d78
ThomasLutaster <lutaster.t@gmail.com>
2021-05-10 11:53:53 +0200

    feat: groups for browse --DONE

--- french_connection/src/Entity/Country.php
+++ french_connection/src/Entity/Country.php
@@ -6,6 +6,7 @@ use App\Repository\CountryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=CountryRepository::class)
@@ -16,21 +17,25 @@ class Country
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @Groups({"country_browse"})
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"country_browse"})
      */
     private $name;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"country_browse"})
      */
     private $countryCode;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"country_browse"})
      */
     private $flag;
 
