52c3ec050afb4632344a38aea26396111f11c54f
ThomasLutaster <lutaster.t@gmail.com>
2021-05-17 17:31:44 +0200

    feat: user-edit-hobby-service --DONE

--- french_connection/src/Entity/Hobby.php
+++ french_connection/src/Entity/Hobby.php
@@ -48,6 +48,10 @@ class Hobby
         $this->users = new ArrayCollection();
     }
 
+    public function __toString() {
+        return $this->name;
+      }
+
     public function getId(): ?int
     {
         return $this->id;
--- french_connection/src/Form/UserEditType.php
+++ french_connection/src/Form/UserEditType.php
@@ -4,16 +4,17 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class UserEditType extends AbstractType
 {
@@ -59,8 +60,8 @@ class UserEditType extends AbstractType
             ])
             //->add('createdAt')
             //->add('updatedAt')
-            //->add('hobbies')
-            //->add('services')
+            ->add('hobbies', null, [])
+            ->add('services', null, [])
             ->add('userAdress', CollectionType::class, [
                 'entry_type' => TextType::class,
                 'allow_add' => true,
